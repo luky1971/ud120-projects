4a40104a7ef3df3daa2b04eb13155eada8e98838
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-07-26 23:45:47 -0400

    Completed svm mini project, updated gitignore

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,5 +26,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+# Cut down data to 1%
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
+clf = svm.SVC(C = 10000.0, kernel = 'rbf')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:",round(time()-t0, 3),"s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:",round(time()-t0, 3),"s"
+
+print "accuracy:",accuracy_score(labels_test, pred)
+
+print "prediction for 10:",pred[10]
+print "prediction for 26:",pred[26]
+print "prediction for 50:",pred[50]
+
+nchris = 0
+for p in pred:
+	if p == 1:
+		nchris += 1
+
+print nchris,"predicted to be Chris"
